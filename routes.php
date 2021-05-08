b0990a32251a716fff6eaf859603e263fea29ff3
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-05-08 12:02:20 -0300

    🧩 New routes and random redirect on homepage

--- routes.php
+++ routes.php
@@ -1,7 +1,15 @@
 <?php
 
 use Components\Router;
-use Components\Application;
 
-Router::route('/', '\Components\Controllers\PageController@getHomepage');
-Router::route('/obrigado', '\Components\Controllers\PageController@getAfterSubscription');
+// Router::route('/', '\Components\Controllers\PageController@getHomepage');
+// Router::route('/obrigado', '\Components\Controllers\PageController@getAfterSubscription');
+
+Router::route('/', function() {
+    $newLocation = ((rand(0, 150000) % 2) != 0) ? 'jogomktdigital' : 'jogomkt';
+    header("Location: /$newLocation", true);
+});
+
+Router::route('/jogomkt', '\Components\Controllers\PageController@marketingDigitalA');
+Router::route('/jogomktdigital', '\Components\Controllers\PageController@marketingDigitalB');
+Router::route('/obrigado', '\Components\Controllers\PageController@obrigado');
