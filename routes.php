9eeef85bbc1c1d930ca36afef6ccde682eeae920
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-09-12 13:19:46 -0300

    🔐 Change default route to hit a new page

--- routes.php
+++ routes.php
@@ -5,11 +5,13 @@ use Components\Router;
 // Router::route('/', '\Components\Controllers\PageController@getHomepage');
 // Router::route('/obrigado', '\Components\Controllers\PageController@getAfterSubscription');
 
-Router::route('/', function() {
-    $newLocation = ((rand(0, 150000) % 2) != 0) ? 'ojogo-mktdigital' : 'jogo-mktdigital';
-    header("Location: /$newLocation", true);
-});
+//Router::route('/', function() {
+//    $newLocation = ((rand(0, 150000) % 2) != 0) ? 'ojogo-mktdigital' : 'jogo-mktdigital';
+//    header("Location: /$newLocation", true);
+//});
 
-Router::route('/jogo-mktdigital', '\Components\Controllers\PageController@marketingDigitalA');
-Router::route('/ojogo-mktdigital', '\Components\Controllers\PageController@marketingDigitalB');
+//Router::route('/jogo-mktdigital', '\Components\Controllers\PageController@marketingDigitalA');
+//Router::route('/ojogo-mktdigital', '\Components\Controllers\PageController@marketingDigitalB');
+
+Router::route('/', '\Components\Controllers\PageController@profissoesMaisBemPagas');
 Router::route('/obrigado', '\Components\Controllers\PageController@obrigado');
