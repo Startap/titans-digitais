addc91ebf320a34366b4af456aa291105f0aea36
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-09-13 16:56:22 -0300

    New template to front website

---
+++ views/profissoes_do_marketing/template.php
@@ -0,0 +1,26 @@
+<article class="article">
+    <section class="headline__container">
+        <h1 class="article__header">
+            <small>As 3 profissões</small>
+            <span>mais bem pagas</span>
+            <small>do Mundo</small>
+        </h1>
+    </section>
+
+    <section id="headline__profissoes">
+        Potencialize seus ganhos financeiros por meio de uma das
+        <strong class="text__accent">três profissões mais bem pagas </strong>
+        e requisitadas que me fizeram
+        <strong class="text__accent">sair de um salário de R$1.500,00 </strong>
+        por mês e gerenciar
+    </section>
+
+    <section id="numeros__lancamento">
+        <p>+ de <strong class="text__accent">1 MILHÃO</strong> investidos em anúncios online</p>
+        <p>+ de <strong class="text__accent">53 lançamentos</strong> de sucesso</p>
+    </section>
+
+    <section id="button__container">
+        Aula exclusiva e gratuita no dia 06 de outubro de 2021.
+    </section>
+</article>
